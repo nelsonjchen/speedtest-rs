608fd3f4397973f1bf0d259d64863b67c8f698dd
Nelson Chen <crazysim@gmail.com>
2015-01-21 01:48:27 -0800

    meh, reader don't work now

--- src/config.rs
+++ src/config.rs
@@ -3,12 +3,22 @@ mod tests {
     extern crate xml;
 
     use std::io::{File, BufferedReader};
+    use xml::reader::EventReader;
+    use xml::reader::events::*;
 
     #[test]
     fn read_xml() {
         let file = File::open(&Path::new("data/speedtest-config.php")).unwrap();
-        let mut reader = BufferedReader::new(file);
+        let reader = BufferedReader::new(file);
 
-        println!("{}", &mut reader.read_to_string().ok().expect("not a string").as_slice());
+        let mut parser = EventReader::new(reader);
+        for e in parser.events() {
+            match e {
+                XmlEvent::StartElement {name, attributes: _, namespace: _ } => {
+                    println!("{}", name);
+                }
+                _ => {}
+            }
+        }
     }
 }
