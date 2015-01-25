59121f70a3852d8d3087f34db290b458f2f51128
Nelson Chen <crazysim@gmail.com>
2015-01-25 14:20:28 -0800

    Screwed up the names heh

--- src/speedtest.rs
+++ src/speedtest.rs
@@ -1,5 +1,23 @@
-mod speedtest;
+#[cfg(test)]
+mod tests {
+    extern crate xml;
+    use std::io::{File, BufferedReader};
+    use self::xml::reader::EventReader;
+    use self::xml::reader::events::*;
 
-fn main() {
-    println!("Hello, world!");
+    #[test]
+    fn read_xml() {
+        let file = File::open(&Path::new("data/speedtest-config.php.xml")).unwrap();
+        let reader = BufferedReader::new(file);
+
+        let mut parser = EventReader::new(reader);
+        for e in parser.events() {
+            println!("{:?}", e);
+            // match e {
+                // XmlEvent::StartElement {name, attributes: attr, namespace: _ } => {
+                // }
+                // _ => {}
+            // }
+        }
+    }
 }
