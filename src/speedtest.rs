fd8a897d28943c084c1c25812b68ebcc0a52bfec
Nelson Chen <crazysim@gmail.com>
2015-02-02 02:53:58 -0800

    Delete XML Test stuff

--- src/speedtest.rs
+++ src/speedtest.rs
@@ -33,27 +33,6 @@ fn fast_earth_distance(origin: EarthLocation, destination: EarthLocation) -> f32
 
 #[cfg(test)]
 mod tests {
-    extern crate xml;
-    use std::io::{File, BufferedReader};
-    use self::xml::reader::EventReader;
-    use self::xml::reader::events::*;
-
-    #[test]
-    fn read_xml() {
-        let file = File::open(&Path::new("data/speedtest-config.php.xml")).unwrap();
-        let reader = BufferedReader::new(file);
-
-        let mut parser = EventReader::new(reader);
-        for e in parser.events() {
-            println!("{:?}", e);
-            // match e {
-                // XmlEvent::StartElement {name, attributes: attr, namespace: _ } => {
-                // }
-                // _ => {}
-            // }
-        }
-    }
-
     #[test]
     fn test_distance() {
 
