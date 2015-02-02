927e3cffc1033d7cd8c9468cba44532f1478502a
Nelson Chen <crazysim@gmail.com>
2015-02-02 00:37:48 -0800

    Implement distance

--- src/speedtest.rs
+++ src/speedtest.rs
@@ -1,5 +1,11 @@
+use std::num::Float;
+
 struct SpeedTestConfig;
 
+struct EarthLocation {
+    latitude: f32,
+    longitude: f32
+}
 
 pub fn speedtest() {
     println!("Starting Speed Test");
@@ -10,8 +16,19 @@ fn get_config() -> SpeedTestConfig {
     SpeedTestConfig
 }
 
-fn distance() {
-
+fn distance(origin: EarthLocation, destination: EarthLocation) -> f32 {
+    let radius: f32 = 6371.0;
+    let d_lat = (origin.latitude - destination.latitude).to_radians();
+    let d_long = (origin.latitude - destination.latitude).to_radians();
+    let a = (d_lat/2.0).sin()
+        * (d_lat/2.0).sin()
+        + origin.latitude.to_radians().cos()
+        * destination.latitude.to_radians().cos()
+        * (d_long/2.0).to_radians().sin()
+        * (d_long/2.0).to_radians().sin();
+    let c = 2.0 * a.sqrt().atan2((1.0 - a).sqrt());
+    let d = radius * c;
+    d
 }
 
 #[cfg(test)]
