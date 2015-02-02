d1d557083f28afa2ee45ed33d7e16fa3a5297212
Nelson Chen <crazysim@gmail.com>
2015-02-02 03:11:07 -0800

    Implement distance calculation

--- src/speedtest.rs
+++ src/speedtest.rs
@@ -2,7 +2,7 @@ use std::num::Float;
 
 struct SpeedTestConfig;
 
-struct EarthLocation {
+pub struct EarthLocation {
     latitude: f32,
     longitude: f32
 }
@@ -16,16 +16,16 @@ fn get_config() -> SpeedTestConfig {
     SpeedTestConfig
 }
 
-fn fast_earth_distance(origin: EarthLocation, destination: EarthLocation) -> f32 {
+pub fn compute_distance(origin: EarthLocation, destination: EarthLocation) -> f32 {
     let radius: f32 = 6371.0;
     let d_lat = (origin.latitude - destination.latitude).to_radians();
-    let d_long = (origin.latitude - destination.latitude).to_radians();
+    let d_long = (origin.longitude - destination.longitude).to_radians();
     let a = (d_lat/2.0).sin()
         * (d_lat/2.0).sin()
         + origin.latitude.to_radians().cos()
         * destination.latitude.to_radians().cos()
-        * (d_long/2.0).to_radians().sin()
-        * (d_long/2.0).to_radians().sin();
+        * (d_long/2.0).sin()
+        * (d_long/2.0).sin();
     let c = 2.0 * a.sqrt().atan2((1.0 - a).sqrt());
     let d = radius * c;
     d
@@ -33,8 +33,16 @@ fn fast_earth_distance(origin: EarthLocation, destination: EarthLocation) -> f32
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::num::Float;
+
     #[test]
     fn test_distance() {
-
+        let origin = EarthLocation { latitude: 32.9545, longitude: -117.2333};
+        let destination = EarthLocation { latitude: 70.0733, longitude: 29.7497};
+        let distance = compute_distance(origin, destination);
+        let diff = (distance - 8255.1).abs();
+        println!("distance: {} diff: {}", distance, diff);
+        assert!(diff < 0.2);
     }
 }
