4c3bff49563c3c3aa50cb78913b5b64595d4da99
Nelson Chen <crazysim@gmail.com>
2015-05-04 23:08:23 -0700

    All thunked out

--- src/speedtest.rs
+++ src/speedtest.rs
@@ -1,43 +1,39 @@
-use xml::{Element, Parser, ElementBuilder};
+// use xml::{Element, Parser, ElementBuilder};
 
-pub struct SpeedTestConfig{
-    ip: String,
-    lat: String,
-    lon: String,
-    isp: String,
-}
+// pub struct SpeedTestConfig{
+//     ip: String,
+//     lat: String,
+//     lon: String,
+//     isp: String,
+// }
 
 
 pub fn run_speedtest() {
     println!("TODO");
 }
 
-pub fn parse_config_xml(config_xml: String) -> SpeedTestConfig {
-    let elem: Element = config_xml.parse().unwrap();
-    SpeedTestConfig{
-        ip: "127.0.0.1".to_string(),
-        lat: "37.4192".to_string(),
-        lon: "-122.0574".to_string(),
-        isp: "Time Warner Cable".to_string(),
-    }
-}
+// pub fn parse_config_xml(config_xml: String) -> SpeedTestConfig {
+//     // let elem: Element = config_xml.parse().unwrap();
+//     SpeedTestConfig{
+//         ip: "127.0.0.1".to_string(),
+//         lat: "0.0".to_string(),
+//         lon: "0.0".to_string(),
+//         isp: "Nipple Massage Cable".to_string(),
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
     use std::io::prelude::*;
     use std::string::String;
+    use std::fs::File;
 
     #[test]
     fn test_parse_config_xml() {
         // TODO: File Reading with try!(File::Open()). Not sure why it considers
+        let mut f = try!(File::open("data/speedtest-config.php.xml"));
         // the result of File::Open() a ().
-        let config_xml_string: String = r#"
-        <?xml version="1.0" encoding="UTF-8"?>
-<settings>
-	<client ip="174.79.12.26" lat="32.9954" lon="-117.0753" isp="Cox Communications" isprating="3.1" rating="0" ispdlavg="18259" ispulavg="5021" loggedin="0"/>
-</settings>
-        "#.to_string();
-        parse_config_xml(config_xml_string);
+        // parse_config_xml(config_xml_string);
     }
 }
