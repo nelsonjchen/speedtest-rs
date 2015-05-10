7aae0a9b2c0cd20a23534d5c4143d4423dd431ac
Nelson Chen <crazysim@gmail.com>
2015-05-09 21:54:39 -0700

    That took forever but it parses, hooray

--- src/speedtest.rs
+++ src/speedtest.rs
@@ -1,5 +1,7 @@
 // use xml::{Element, Parser, ElementBuilder};
 use std::io::Read;
+use xml::reader::EventReader;
+use xml::reader::events::XmlEvent::*;
 
 #[derive(Debug)]
 pub struct ParseError(String);
@@ -12,8 +14,55 @@ pub struct SpeedTestConfig {
 }
 
 impl SpeedTestConfig {
-    fn new<R: Read(parser: &mut EventReader<R>) -> Result<SpeedTestConfig, ParseError> {
-        unimplemented!();
+    fn new<R: Read>(parser: &mut EventReader<R>) -> Result<SpeedTestConfig, ParseError> {
+        let mut ip: Option<String> = None;
+        let mut lat: Option<String> = None;
+        let mut lon: Option<String> = None;
+        let mut isp: Option<String> = None;
+        for e in parser.events() {
+            match e {
+                StartElement { ref name, ref attributes, ..} => {
+                    match name.local_name.as_ref() {
+                        "client" => {
+                            for item in attributes {
+                                match item.name.local_name.as_ref() {
+                                    "ip" => {
+                                        ip = Some(item.value.clone());
+                                    },
+                                    "lat" => {
+                                        lat = Some(item.value.clone());
+                                    },
+                                    "lon" => {
+                                        lon = Some(item.value.clone());
+                                    },
+                                    "isp" => {
+                                        isp = Some(item.value.clone());
+                                    },
+                                    _ => {},
+
+                                }
+                            }
+                        break;
+                        },
+                        _ => {}
+                    }
+                },
+                _ => {}
+            }
+        }
+        match (ip, lat, lon, isp) {
+            (Some(ip), Some(lat), Some(lon), Some(isp))=> {
+            return Ok(SpeedTestConfig {
+                ip: ip,
+                lat: lat,
+                lon: lon,
+                isp: isp,
+                })
+            },
+            _ => {
+                return Err(ParseError("something is missing".to_string()));
+            }
+        }
     }
 }
 
@@ -25,14 +74,15 @@ pub fn run_speedtest() {
 mod tests {
     use super::*;
     use std::io::prelude::*;
+    use std::io::Cursor;
     use std::string::String;
     use std::fs::File;
+    use xml::reader::EventReader;
 
     #[test]
     fn test_parse_config_xml() {
-        unimplemented!();
-        let config_xml = include_bytes!("../tests/data/speedtest-config.php.xml");
-        let config = parse_config_xml(config_xml).unwrap();
+        let mut parser = EventReader::new(include_bytes!("../tests/data/speedtest-config.php.xml") as &[u8]);
+        let config = SpeedTestConfig::new(&mut parser).unwrap();
         assert_eq!("174.79.12.26", config.ip);
     }
 }
