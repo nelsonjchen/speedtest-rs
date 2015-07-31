fc7b59b93e207a16d36b9cc46c12c347c1fa6622
Nelson Chen <crazysim@gmail.com>
2015-07-30 23:56:19 -0700

    Add logging support

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ extern crate hyper;
 extern crate xml;
 extern crate time;
 
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 #[allow(dead_code)]
 fn main() {
     speedtest::run_speedtest();
