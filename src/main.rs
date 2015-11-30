ea4034bc44299ec771b1978a50fe8b05802d35a8
Nelson Chen <crazysim@gmail.com>
2015-11-29 20:08:14 -0800

    Add simple flag and UI

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,18 @@ fn main() {
                       .arg(Arg::with_name("bytes")
                                .long("bytes")
                                .help(" Display values in bytes instead of bits."))
+                      .arg(Arg::with_name("simple")
+                               .long("simple")
+                               .help("Suppress verbose output, only show basic informatio"))
                       .get_matches();
 
-    println!("Retrieving speedtest.net configuration...", );
+    if !matches.is_present("simple") {
+        println!("Retrieving speedtest.net configuration...");
+    }
     let config = speedtest::get_configuration().unwrap();
-    println!("Retrieving speedtest.net server list...", );
+    if !matches.is_present("simple") {
+        println!("Retrieving speedtest.net server list...");
+    }
     let server_list = speedtest::get_server_list_with_config(Some(&config)).unwrap();
     let mut server_list_sorted = server_list.servers_sorted_by_distance(&config);
 
@@ -45,8 +52,10 @@ fn main() {
         }
         return;
     }
-    println!("Testing from {} ({})...", config.isp, config.ip);
-    println!("Selecting best server based on latency...");
+    if !matches.is_present("simple") {
+        println!("Testing from {} ({})...", config.isp, config.ip);
+        println!("Selecting best server based on latency...");
+    }
     info!("Five Closest Servers");
     server_list_sorted.truncate(5);
     for server in &server_list_sorted {
@@ -54,18 +63,34 @@ fn main() {
     }
     let latecy_test_result = speedtest::get_best_server_based_on_latency(&server_list_sorted[..])
                                  .unwrap();
-    println!("Hosted by {} ({}) [{:.2} km]: {}.{} ms",
+    if !matches.is_present("simple") {
+        println!("Hosted by {} ({}) [{:.2} km]: {}.{} ms",
              latecy_test_result.server.sponsor,
              latecy_test_result.server.name,
              latecy_test_result.server.distance.unwrap(),
              latecy_test_result.latency.num_milliseconds(),
              latecy_test_result.latency.num_microseconds().unwrap() % 1000,
          );
+    } else {
+        println!("Ping: {}.{} ms",
+            latecy_test_result.latency.num_milliseconds(),
+            latecy_test_result.latency.num_microseconds().unwrap() % 1000,
+        );
+    }
     let best_server = latecy_test_result.server;
-    print!("Testing download speed");
-    let download_measurement = speedtest::test_download_with_progress(best_server, print_dot)
+
+    let download_measurement;
+
+    if !matches.is_present("simple") {
+        print!("Testing download speed");
+        download_measurement = speedtest::test_download_with_progress(best_server, print_dot)
                                    .unwrap();
-    println!("");
+        println!("");
+    } else {
+        download_measurement = speedtest::test_download_with_progress(best_server, do_nothing)
+                                   .unwrap();
+    }
+
     if matches.is_present("bytes") {
         println!("Download: {:.2} Mbyte/s",
                  (download_measurement.size / 800) as f32 /
@@ -75,9 +100,17 @@ fn main() {
                  (download_measurement.size / 100) as f32 /
                  download_measurement.duration.num_milliseconds() as f32);
     }
-    print!("Testing upload speed");
-    let upload_measurement = speedtest::test_upload_with_progress(best_server, print_dot).unwrap();
-    println!("");
+
+    let upload_measurement;
+
+    if !matches.is_present("simple") {
+        print!("Testing upload speed");
+        upload_measurement = speedtest::test_upload_with_progress(best_server, print_dot).unwrap();
+        println!("");
+    } else {
+        upload_measurement = speedtest::test_upload_with_progress(best_server, do_nothing).unwrap();
+    }
+
     if matches.is_present("bytes") {
         println!("Upload: {:.2} Mbyte/s",
                  (upload_measurement.size / 800) as f32 /
@@ -92,4 +125,7 @@ fn main() {
 fn print_dot() {
     print!(".");
     io::stdout().flush().unwrap();
+}
+
+fn do_nothing() {
 }
