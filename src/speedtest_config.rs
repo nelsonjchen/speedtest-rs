5a3711a92b6e1c4809b570b33c4d4ebbc4d76b30
Nelson Chen <crazysim@gmail.com>
2020-07-13 22:45:47 -0700

    Complete imports

--- src/speedtest_config.rs
+++ src/speedtest_config.rs
@@ -1,11 +1,17 @@
+use crate::{error::Error, distance::EarthLocation};
+use std::io::Read;
+use xml::EventReader;
+use xml::reader::XmlEvent::StartElement;
+
+
 pub struct SpeedTestConfig {
     pub ip: String,
-    location: EarthLocation,
+    pub location: EarthLocation,
     pub isp: String,
 }
 
 impl SpeedTestConfig {
-    fn new<R: Read>(parser: EventReader<R>) -> Result<SpeedTestConfig, Error> {
+    pub fn new<R: Read>(parser: EventReader<R>) -> Result<SpeedTestConfig, Error> {
         let mut ip: Option<String> = None;
         let mut lat: Option<f32> = None;
         let mut lon: Option<f32> = None;
@@ -47,5 +53,25 @@ impl SpeedTestConfig {
         } else {
             Err(Error::ConfigParseError)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use xml::EventReader;
+    #[test]
+    fn test_parse_config_xml() {
+        let parser = EventReader::new(include_bytes!("../tests/config/config.php.xml") as &[u8]);
+        let config = SpeedTestConfig::new(parser).unwrap();
+        assert_eq!("174.79.12.26", config.ip);
+        assert_eq!(
+            EarthLocation {
+                latitude: 32.9954,
+                longitude: -117.0753,
+            },
+            config.location
+        );
+        assert_eq!("Cox Communications", config.isp);
     }
 }
