9ad80aba9840734994dfa863e725e6bb2a069efa
Mahmudul Hasan <101699736+Consistent-Milk@users.noreply.github.com>
2023-12-05 22:36:37 +0600

    updated to clap v4

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod speedtest_servers_config;
 
 use crate::speedtest_csv::SpeedTestCsvResult;
 use chrono::Utc;
-use clap::{crate_version, App, Arg};
+use clap::{crate_version, value_parser, Arg, ArgAction, Command};
 use log::info;
 use std::io::{self, Write};
 use url::Url;
@@ -15,94 +15,106 @@ use url::Url;
 fn main() -> Result<(), error::SpeedTestError> {
     env_logger::init();
 
-    let matches = App::new("speedtest-rs")
+    let matches = Command::new("speedtest-rs")
         .version(crate_version!())
+        .propagate_version(true)
         .about("Command line interface for testing internet bandwidth using speedtest.net.")
         .arg(
-            Arg::with_name("no-download")
+            Arg::new("no-download")
+                .action(ArgAction::SetTrue)
                 .long("no-download")
                 .help("Don't run download test"),
         )
         .arg(
-            Arg::with_name("no-upload")
+            Arg::new("no-upload")
+                .action(ArgAction::SetTrue)
                 .long("no-upload")
                 .help("Don't run upload test"),
         )
         .arg(
-            Arg::with_name("list")
+            Arg::new("list")
+                .action(ArgAction::SetTrue)
                 .long("list")
                 .help("Display a list of speedtest.net servers sorted by distance"),
         )
         .arg(
-            Arg::with_name("share")
+            Arg::new("share")
+                .action(ArgAction::SetTrue)
                 .long("share")
                 .help("Generate and provide an URL to the speedtest.net share results image"),
         )
         .arg(
-            Arg::with_name("bytes")
+            Arg::new("bytes")
+                .action(ArgAction::SetTrue)
                 .long("bytes")
                 .help("Display values in bytes instead of bits."),
         )
         .arg(
-            Arg::with_name("simple")
+            Arg::new("simple")
+                .action(ArgAction::SetTrue)
                 .long("simple")
                 .help("Suppress verbose output, only show basic information"),
         )
-        .arg(Arg::with_name("csv").long("csv").help(
+        .arg(Arg::new("csv").action(ArgAction::SetTrue).long("csv").help(
             "Suppress verbose output, only show basic information in CSV format.\
              Speeds listed in bit/s and not affected by --bytes",
         ))
         .arg(
-            Arg::with_name("csv-header")
+            Arg::new("csv-header")
+                .action(ArgAction::SetTrue)
                 .long("csv-header")
                 .help("Print CSV headers"),
         )
         .arg(
-            Arg::with_name("csv-delimiter")
-                .long("csv-delimiter")
+            Arg::new("csv-delimeter")
+                .value_name("csv-delimeter")
+                .long("csv-delimeter")
                 .help("Single character delimiter to use in CSV output.")
-                .takes_value(true)
+                .num_args(1)
                 .default_value(",")
-                .validator(|v| {
-                    if v.chars().count() == 1 {
-                        Ok(())
-                    } else {
-                        Err("--csv-delimiter must be a single character".into())
-                    }
-                }),
+                .value_parser(clap::builder::ValueParser::new(validate_csv_delimeter)),
         )
         .arg(
-            Arg::with_name("mini")
+            Arg::new("mini")
+                .value_name("mini")
                 .long("mini")
                 .help("Address of speedtest-mini server")
-                .takes_value(true),
+                .num_args(1)
+                .value_parser(value_parser!(String))
+                .required(false),
         )
         .get_matches();
 
     // This appears to be purely informational.
-    if matches.is_present("csv-header") {
+    if matches.get_flag("csv-header") {
         let results = speedtest_csv::SpeedTestCsvResult::default();
 
         println!("{}", results.header_serialize());
         return Ok(());
     }
 
-    let machine_format = matches.is_present("csv") || matches.is_present("json");
+    let machine_format = matches.get_flag("csv");
 
-    if !matches.is_present("simple") && !machine_format {
+    if !matches.get_flag("simple") && !machine_format {
         println!("Retrieving speedtest.net configuration...");
     }
     let mut config = speedtest::get_configuration()?;
 
     let mut server_list_sorted;
-    if !matches.is_present("mini") {
-        if !matches.is_present("simple") && !machine_format {
+
+    let mini: String = match matches.contains_id("mini") {
+        true => matches.get_one::<String>("mini").unwrap().to_string(),
+        false => "".to_string(),
+    };
+
+    if mini.is_empty() {
+        if !matches.get_flag("simple") && !machine_format {
             println!("Retrieving speedtest.net server list...");
         }
         let server_list = speedtest::get_server_list_with_config(&config)?;
         server_list_sorted = server_list.servers_sorted_by_distance(&config);
 
-        if matches.is_present("list") {
+        if matches.get_flag("list") {
             for server in server_list_sorted {
                 println!(
                     "{:4}) {} ({}, {}) [{}]",
@@ -117,7 +129,7 @@ fn main() -> Result<(), error::SpeedTestError> {
             }
             return Ok(());
         }
-        if !matches.is_present("simple") && !machine_format {
+        if !matches.get_flag("simple") && !machine_format {
             println!(
                 "Testing from {} ({})...",
                 config.client.isp, config.client.ip
@@ -131,8 +143,7 @@ fn main() -> Result<(), error::SpeedTestError> {
             info!("Close Server: {server:?}");
         }
     } else {
-        let mini = matches.value_of("mini").unwrap();
-        let mini_url = Url::parse(mini).unwrap();
+        let mini_url = Url::parse(&mini).unwrap();
 
         // matches.value_of("mini").unwrap().to_string()
 
@@ -168,7 +179,7 @@ fn main() -> Result<(), error::SpeedTestError> {
     let latency_test_result = speedtest::get_best_server_based_on_latency(&server_list_sorted[..])?;
 
     if !machine_format {
-        if !matches.is_present("simple") {
+        if !matches.get_flag("simple") {
             println!(
                 "Hosted by {} ({}){}: {}.{} ms",
                 latency_test_result.server.sponsor,
@@ -194,8 +205,8 @@ fn main() -> Result<(), error::SpeedTestError> {
     let download_measurement;
     let inner_download_measurement;
 
-    if !matches.is_present("no-download") {
-        if !matches.is_present("simple") && !machine_format {
+    if !matches.get_flag("no-download") {
+        if !matches.get_flag("simple") && !machine_format {
             print!("Testing download speed");
             inner_download_measurement = speedtest::test_download_with_progress_and_config(
                 best_server,
@@ -209,7 +220,7 @@ fn main() -> Result<(), error::SpeedTestError> {
         }
 
         if !machine_format {
-            if matches.is_present("bytes") {
+            if matches.get_flag("bytes") {
                 println!(
                     "Download: {:.2} Mbyte/s",
                     ((inner_download_measurement.kbps() / 8) as f32 / 1000.00)
@@ -229,8 +240,8 @@ fn main() -> Result<(), error::SpeedTestError> {
     let upload_measurement;
     let inner_upload_measurement;
 
-    if !matches.is_present("no-upload") {
-        if !matches.is_present("simple") && !machine_format {
+    if !matches.get_flag("no-upload") {
+        if !matches.get_flag("simple") && !machine_format {
             print!("Testing upload speed");
             inner_upload_measurement =
                 speedtest::test_upload_with_progress_and_config(best_server, print_dot, &config)?;
@@ -241,7 +252,7 @@ fn main() -> Result<(), error::SpeedTestError> {
         }
 
         if !machine_format {
-            if matches.is_present("bytes") {
+            if matches.get_flag("bytes") {
                 println!(
                     "Upload: {:.2} Mbyte/s",
                     ((inner_upload_measurement.kbps() / 8) as f32 / 1000.00)
@@ -265,7 +276,7 @@ fn main() -> Result<(), error::SpeedTestError> {
         latency_measurement: &latency_test_result,
     };
 
-    if matches.is_present("csv") {
+    if matches.get_flag("csv") {
         let speedtest_csv_result = SpeedTestCsvResult {
             server_id: &best_server.id.to_string(),
             sponsor: &best_server.sponsor,
@@ -284,30 +295,29 @@ fn main() -> Result<(), error::SpeedTestError> {
                 .map_or(0.0, |x| x.bps_f64())
                 .to_string(),
             upload: &upload_measurement.map_or(0.0, |x| x.bps_f64()).to_string(),
-            share: &if matches.is_present("share") {
+            share: &if matches.get_flag("share") {
                 speedtest::get_share_url(&speedtest_result)?
             } else {
                 "".to_string()
             },
             ip_address: &config.client.ip.to_string(),
         };
+
+        let delim: String = matches
+            .get_one("csv-delimeter")
+            .unwrap_or(&",".to_string())
+            .to_string();
+
         let mut wtr = csv::WriterBuilder::new()
             .has_headers(false)
-            .delimiter(
-                matches
-                    .value_of("csv-delimiter")
-                    .unwrap_or(",")
-                    .chars()
-                    .next()
-                    .unwrap_or(',') as u8,
-            )
+            .delimiter(delim.chars().next().unwrap_or(',') as u8)
             .from_writer(io::stdout());
         wtr.serialize(speedtest_csv_result)?;
         wtr.flush()?;
         return Ok(());
     }
 
-    if matches.is_present("share") && !machine_format {
+    if matches.get_flag("share") && !machine_format {
         info!("Share Request {speedtest_result:?}",);
         println!(
             "Share results: {}",
@@ -331,4 +341,12 @@ fn main() -> Result<(), error::SpeedTestError> {
 fn print_dot() {
     print!(".");
     io::stdout().flush().unwrap();
+}
+
+fn validate_csv_delimeter(arg: &str) -> Result<String, String> {
+    if arg.len() == 1 {
+        Ok(arg.to_string())
+    } else {
+        Err("--csv-delimiter must be a single character".into())
+    }
 }
